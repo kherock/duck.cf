1653969f50d00937cef14f6eb616783840a73e80
Alexander Honer <ahoner@umd.edu>
2016-10-03 14:25:02 -0400

    more ducks, quack

--- web/app/ran-d-uck/ran-d-uck.ts
+++ web/app/ran-d-uck/ran-d-uck.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <img [src]="duckUrl">
     <div>
-        <button md-raised-button disableRipple color="primary" (click)="randuck()">Duck</button>
+        <button md-raised-button disableRipple color="primary" (click)="randuck()">Ran-d-uck</button>
     </div>
   `,
   styleUrls: ['ran-d-uck.scss']
@@ -14,7 +14,12 @@ export class RanDuck {
   static DUCK_URLS: string[] = [
     'http://web.stanford.edu/dept/CTL/cgi-bin/academicskillscoaching/wp-content/uploads/2012/07/baby-duck.jpg',
     'https://upload.wikimedia.org/wikipedia/commons/9/96/Domestic-crested-duck-CamdenME.jpg',
-    'http://i2.cdn.turner.com/cnnnext/dam/assets/130924135602-taiwan-duck-5-horizontal-large-gallery.jpg'
+    'http://i2.cdn.turner.com/cnnnext/dam/assets/130924135602-taiwan-duck-5-horizontal-large-gallery.jpg',
+    'http://luckyduckrescue.org/IMG_0049.jpg',
+    'http://c3405147.r47.cf0.rackcdn.com/waterfowlID/fullSize/1/blackbelliedwhistlingduck1.jpg',
+    'https://s-media-cache-ak0.pinimg.com/236x/aa/9f/62/aa9f62a5dedeb3dfe733362b3f0ee885.jpg',
+    'http://notyourmommascookie.com/wp-content/uploads/2012/02/confused-duck.jpg',
+    'http://www.permacultureproject.com/wp-content/uploads/2011/01/Paranoid-Duck-large.jpg'
   ];
   duckUrl: string = this.randuck();
 
